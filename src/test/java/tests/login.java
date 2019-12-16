330fc603cd12fa6b9d2dad8d2b1078eb3665a224
Demir Salih GURSOY <slh.gursoy@hotmail.com>
2019-12-16 21:20:32 +0100

    IDE added line 6

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -3,6 +3,7 @@ package tests;
 public class login {
     public static void main(String[] args) {
         // this is written from IntelliJ IDE
+        System.out.println("Print from Intellij IDE");
     }
     
     //added from GitHub
